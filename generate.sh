978b3f014bc4f4c51f4d9c6b7fae308ad4d99705
Richard Pappalardo <rpappalax@gmail.com>
2020-09-03 21:22:34 -0700

    fix: .gitignore

--- generate.sh
+++ generate.sh
@@ -3,12 +3,14 @@
 CREATION_DATE=1599087616271
 IN_FILE=1000_urls.txt
 OUT_FILE=insert.sql
+DB_IN_FILE=browser.db
 
 function generate_sql() {
-    ROW="INSERT INTO tabs (url, title, history, last_used) VALUES (\"https://${SITE}\", \"${SITE}\", "[\"https:\/\/${SITE}\/\"]", ${CREATION_DATE});"
+    ROW="INSERT INTO tabs (url, title, history, last_used) VALUES (\"https://${SITE}\", \"${SITE}\", "[\"https:\\\/\\\/${SITE}\\\/\"]", ${CREATION_DATE});"
     echo $ROW
 }
 
 rm -f ${OUT_FILE}
 while read SITE; do generate_sql; done < ./${IN_FILE} >>  ${OUT_FILE}
+while read ROW; do sqlite3 ${DB_IN_FILE}; done < ./${OUT_FILE}
 
