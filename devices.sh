e4e2c6652516d9d255ff4d93f5ed171fe0ba9b4d
Richard Pappalardo <rpappalax@gmail.com>
2020-09-03 23:10:36 -0700

    feat: add db data hydration (WIP)

--- devices.sh
+++ devices.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-DEVICE_ID="$(xcrun simctl list | grep Booted | awk -F '[()]' '{print $4}')"
+# This purpose of this script is to create easy access
+# to Firefox iOS databases so they can be pre-populated 
+# with dummy data for testing
+
+# Does the following:
+
+# 1. Look for a "Booted" iOS simulator device
+# 2. Identify the Firefox iOS DB paths
+# #. Open a Finder window to each
+
+DEVICE_ID="$(xcrun simctl list | grep Booted | awk -F '[()]' '{print $4}')";
+
+echo
+echo
+if [ -z "$DEVICE_ID" ]; then
+    echo "-------------------------------"
+    echo "ERROR"
+    echo "-------------------------------"
+    echo "No booted device found. Aborting!"
+    echo
+    echo
+    exit 0
+fi
+
 DEVICE_PATH=~/Library/Developer/CoreSimulator/Devices/${DEVICE_ID}/data/Containers/
 
-PROFILE_DB=$(find ${DEVICE_PATH} . -type d -name "profile.profile")
-TEST_PROFILE_DB=$(find ${DEVICE_PATH} . -type d -name "profile.testProfile")
-TEST_FIXTURES_DB=$(find ${DEVICE_PATH} . -type d -name "test-fixtures")
+PATH_PROFILE_DB=$(find ${DEVICE_PATH} . -type d -name "profile.profile")
+PATH_TEST_PROFILE_DB=$(find ${DEVICE_PATH} . -type d -name "profile.testProfile")
+PATH_TEST_FIXTURES_DB=$(find ${DEVICE_PATH} . -type d -name "test-fixtures")
 
 
 echo "-------------------------------"
@@ -23,21 +46,26 @@ echo $DEVICE_PATH
 
 echo
 echo "-------------------------------"
-echo "PROFILE_DB"
+echo "PATH_PROFILE_DB"
 echo "-------------------------------"
 
-echo $PROFILE_DB
-
-echo
-echo "-------------------------------"
-echo "TEST_PROFILE_DB"
-echo "-------------------------------"
-
-echo $TEST_PROFILE_DB
+echo $PATH_PROFILE_DB
+open $PATH_PROFILE_DB
 
 echo
 echo "-------------------------------"
-echo "TEST_FIXTURES_DB"
+echo "PATH_TEST_PROFILE_DB"
 echo "-------------------------------"
 
-echo $TEST_FIXTURES_DB
+echo $PATH_TEST_PROFILE_DB
+open $PATH_TEST_PROFILE_DB
+
+echo
+echo "-------------------------------"
+echo "PATH_TEST_FIXTURES_DB"
+echo "-------------------------------"
+
+echo $PATH_TEST_FIXTURES_DB
+open $PATH_TEST_FIXTURES_DB
+echo
+echo
--- generate.sh
+++ generate.sh
@@ -5,12 +5,57 @@ IN_FILE=1000_urls.txt
 OUT_FILE=insert.sql
 DB_IN_FILE=browser.db
 
+
+rm -f ${OUT_FILE}
+
+# set row numbers for each test DB desired
+if [ "$#" -eq 0 ]; then
+    counters=( 10 20 40 )
+else
+    counters=( "$@" )
+fi
+
 function generate_sql() {
-    ROW="INSERT INTO tabs (url, title, history, last_used) VALUES (\"https://${SITE}\", \"${SITE}\", "[\"https:\\\/\\\/${SITE}\\\/\"]", ${CREATION_DATE});"
+    ROW="INSERT INTO tabs (url, title, history, last_used) VALUES (\"https://${SITE}\", \"${SITE}\", \'[\"https:\\\/\\\/${SITE}\\\/\"]\', ${CREATION_DATE});"
     echo $ROW
 }
 
-rm -f ${OUT_FILE}
+echo
+echo
+echo "---------------------------"
+echo "GENERATE SQL FROM SITE LIST"
+echo "---------------------------"
+echo
 while read SITE; do generate_sql; done < ./${IN_FILE} >>  ${OUT_FILE}
-while read ROW; do sqlite3 ${DB_IN_FILE}; done < ./${OUT_FILE}
+
+echo
+echo "---------------------------"
+echo "CREATE NEW ${DB_IN_FILE}"
+echo "---------------------------"
+echo
+# remove file ext
+DB_NAME="$(cut -d'.' -f1 <<< ${DB_IN_FILE})"
+for counter in "${counters[@]}"
+do  
+    cp ${DB_IN_FILE} "${DB_NAME}_${counter}.db"
+done
+
+for counter in "${counters[@]}"
+do  
+    echo
+    echo
+    echo "---------------------------"
+    echo "HYDRATE ${DB_IN_FILE}"
+    echo "---------------------------"
+    echo
+    NEW_DB_IN_FILE="${DB_NAME}_${counter}.db"
+    cp ${DB_IN_FILE} ${NEW_DB_IN_FILE} 
+    for row in ${counter}
+    do
+        LINE=`sed -n "${row}p" ${IN_FILE}`
+        echo "LINE: ${LINE}"
+        sqlite3 ${NEW_DB_IN_FILE} "${LINE}"
+    done
+done
+#while read ROW; do sqlite3 ${NEW_DB_IN_FILE} "${ROW}"; done < ./${OUT_FILE}
 
